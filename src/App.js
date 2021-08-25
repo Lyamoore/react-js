6fbd4c8f686a2ec72575627db3b6c439f156ef00
lyamoore <lyamooore@mail.ru>
2021-08-25 21:02:16 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import { useState, useCallback, useEffect } from "react";
-import { Form } from "./components/form";
-import { MessageList } from "./components/message-list";
 import "./App.css";
 
 const App = () => {
-  const [name, setName] = useState("");
-
-  useEffect(() => {
-    /// settimeout
-
-    return () => {};
-  }, []);
-
-  const handleNameChange = useCallback((e) => {
-    setName(e.target.value);
-  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Form name={name} handleNameChange={handleNameChange}/>
-        <MessageList />
-      </header>
+
     </div>
   );
 };
--- src/components/message-list/messageList.js
+++ src/components/message-list/messageList.js
@@ -1,13 +1,3 @@
-import React, { useState } from "react";
-
 export const MessageList = () => {
-  const [list] = useState([3, 3, 4]);
 
-  return (
-    <>
-      {list.map((element, index) => {
-        return <React.Fragment key={index}>{element}</React.Fragment>;
-      })}
-    </>
-  );
 };
